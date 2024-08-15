759a02bf27d868e1f9efbaa93711603f0dfc2e7e
jva <vjojo@protonmail.com>
2024-08-15 20:35:52 +0200

    fix version mismatch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "tibarj.tranquilstopwatch"
         minSdk = 30
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.1"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
