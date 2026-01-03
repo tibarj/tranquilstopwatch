78b55cb420dd4f96a06475e8a1b2e521875cb25a
jva <vjojo@protonmail.com>
2026-01-03 19:01:07 +0100

    fix about and placeholder strings

--- app/src/main/java/tibarj/tranquilstopwatch/AboutActivity.kt
+++ app/src/main/java/tibarj/tranquilstopwatch/AboutActivity.kt
@@ -2,15 +2,21 @@ package tibarj.tranquilstopwatch
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import tibarj.tranquilstopwatch.databinding.AboutActivityBinding
 
 class AboutActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: AboutActivityBinding
+    private val navController by lazy {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_about) as NavHostFragment
+        navHostFragment.navController
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,15 +24,21 @@ class AboutActivity : AppCompatActivity() {
         binding = AboutActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_about)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        // Force showing an Up button even if the graph has a single start destination.
+        appBarConfiguration = AppBarConfiguration(setOf())
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        title = getString(R.string.actionbar_about)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_about)
         return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+                || run {
+                    onBackPressedDispatcher.onBackPressed()
+                    true
+                }
     }
 }
